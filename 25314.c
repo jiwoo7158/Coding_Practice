eaedcc59e1d99dbbac631b95dcce7deb03d203a2
jiwoo7158 <jiwoo7158@gmail.com>
2023-03-08 22:18:03 +0900

    [update] 백준 25314번

---
+++ 25314.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int num = 0;
+    scanf("%d", &num);
+
+    int count = 0;
+    count = num / 4;
+    for (int i = 0; i < count; i++){
+        printf("long ");
+
+    }
+
+
+    printf("int");
+    return 0;
+}
