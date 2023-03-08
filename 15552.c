8965a6aea073d8f15b2c914f874fd51822fcf2f5
jiwoo7158 <jiwoo7158@gmail.com>
2023-03-08 22:17:50 +0900

    [update] 백준 15552번

---
+++ 15552.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num = 0;
+    scanf("%d", &num);
+    for(int i = 0; i < num; i++){
+        int a = 0, b = 0;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
